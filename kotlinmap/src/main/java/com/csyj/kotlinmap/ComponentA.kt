6bf02c3ab33a98891bb1c354461465b81e0c511e
wangcaisheng <king.wang@nf-3.com>
2019-10-23 18:31:29 +0800

    CC组件化搭建

---
+++ kotlinmap/src/main/java/com/csyj/kotlinmap/ComponentA.kt
@@ -0,0 +1,49 @@
+package com.csyj.kotlinmap
+
+import com.billy.cc.core.component.CC
+import com.billy.cc.core.component.CCResult
+import com.billy.cc.core.component.CCUtil
+import com.billy.cc.core.component.IComponent
+
+class ComponentA : IComponent{
+    override fun onCall(cc: CC?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val actionName:String=cc!!.actionName
+        when (actionName){
+            "showActivity" ->{
+                //跳转到页面：ActivityA
+                CCUtil.navigateTo(cc, MapsActivity::class.java)
+                //返回处理结果给调用方
+                CC.sendCCResult(cc.getCallId(), CCResult.success())
+                return true
+            }
+            else->{
+                CC.sendCCResult(cc.getCallId(), CCResult.errorUnsupportedActionName())
+                return false
+            }
+
+
+//        String actionName = cc.getActionName()
+//        switch (actionName) {
+//            case "showActivity": //响应actionName为"showActivity"的组件调用
+//            //跳转到页面：ActivityA
+//            CCUtil.navigateTo(cc, ActivityA.class)
+//            //返回处理结果给调用方
+//            CC.sendCCResult(cc.getCallId(), CCResult.success())
+//            //同步方式实现（在return之前听过CC.sendCCResult()返回组件调用结果），return false
+//            return false
+//            default:
+//            //其它actionName当前组件暂时不能响应，可以通过如下方式返回状态码为-12的CCResult给调用方
+//            CC.sendCCResult(cc.getCallId(), CCResult.errorUnsupportedActionName());
+//            return false;
+        }
+//    }
+    }
+
+    override fun getName(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "ComponentA"
+    }
+
+}
